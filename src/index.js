92a5e358f7abd2bc988e97dc414700854672f865
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-01-19 09:55:46 +0700

    database init with prisma

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 const app = express();
 
 dotenv.config();
@@ -7,9 +10,17 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.get("/api", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World!, Ini aku express JS");
+});
+
+
+app.get("/products", async (req, res) => {
+    const products = await prisma.product.findMany();
+
+    res.send(products);
 })
+
 
 app.listen(PORT, () => {
     console.log("Express running in port: " + PORT);
-})
+});
