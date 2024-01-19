b765acbe69b8be5528fff0b38bbd7d4e33234766
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-01-19 10:26:47 +0700

    post and delete products

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.get("/api", (req, res) => {
     res.send("Hello World!, Ini aku express JS");
 });
@@ -18,8 +20,37 @@ app.get("/products", async (req, res) => {
     const products = await prisma.product.findMany();
 
     res.send(products);
-})
+});
 
+app.post("/products", async (req, res) => {
+    const newProductData = req.body;
+
+    const product = await prisma.product.create({
+        data : {
+            name : newProductData.name,
+            description : newProductData.description,
+            price : newProductData.price,
+            image: newProductData.image
+        }
+    });
+
+    res.status(201).send({
+        data: product,
+        message: "Product successfully created"
+    });
+});
+
+app.delete("/products/:id", async (req, res) => {
+    const productId = req.params.id
+
+    await prisma.product.delete({
+        where: {
+            id: parseInt(productId)
+        }
+    });
+
+    res.send("Product successfully deleted");
+});
 
 app.listen(PORT, () => {
     console.log("Express running in port: " + PORT);
