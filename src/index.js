e79472730322452be6bea88f9bab5c51b897476f
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-01-19 10:49:02 +0700

    put and patch api endpoint products

--- src/index.js
+++ src/index.js
@@ -26,10 +26,10 @@ app.post("/products", async (req, res) => {
     const newProductData = req.body;
 
     const product = await prisma.product.create({
-        data : {
-            name : newProductData.name,
-            description : newProductData.description,
-            price : newProductData.price,
+        data: {
+            name: newProductData.name,
+            description: newProductData.description,
+            price: newProductData.price,
             image: newProductData.image
         }
     });
@@ -50,6 +50,54 @@ app.delete("/products/:id", async (req, res) => {
     });
 
     res.send("Product successfully deleted");
+});
+
+app.put("/products/:id", async (req, res) => {
+    const productId = req.params.id;
+    const productData = req.body;
+
+    if (!(productData.image && productData.description && productData.name && productData.price)) {
+        return res.status(400).send("Some fields are missing");
+    }
+
+    const product = await prisma.product.update({
+        where: {
+            id: parseInt(productId)
+        },
+        data: {
+            name: productData.name,
+            description: productData.description,
+            price: productData.price,
+            image: productData.image
+        }
+    });
+
+    res.send({
+        data: product,
+        message: "Product Succesfully edited"
+    })
+});
+
+app.patch("/products/:id", async (req, res) => {
+    const productId = req.params.id;
+    const productData = req.body;
+
+    const product = await prisma.product.update({
+        where: {
+            id: parseInt(productId)
+        },
+        data: {
+            name: productData.name,
+            description: productData.description,
+            price: productData.price,
+            image: productData.image
+        }
+    });
+
+    res.send({
+        data: product,
+        message: "Product Succesfully edited"
+    })
 });
 
 app.listen(PORT, () => {
